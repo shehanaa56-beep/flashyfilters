b9e485a77aa12415c7c92db7d1f4c10d5356ec96
shehanaa56-beep <shehanaa56@gmail.com>
2025-12-11 18:07:09 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-// Register service worker for PWA
-serviceWorkerRegistration.register();
+// Removed service worker completely
 
 reportWebVitals();
